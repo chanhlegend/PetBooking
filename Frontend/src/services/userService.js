58fa8c87350f882315069beb3b3b719c8f646d96
Minh Hiển <minhhien30201@gmail.com>
2025-05-17 00:18:36 +0700

    Register and verify

--- Frontend/src/services/userService.js
+++ Frontend/src/services/userService.js
@@ -22,20 +22,56 @@ export const UserService = {
   },
 
   register: async (fullname, email, password) => {
-    const res = await fetch (`${API_URL}/register`, {
-      method : "POST",
+    const res = await fetch(`${API_URL}/register`, {
+      method: "POST",
       headers: {
-        "Content-Type" : "application/json",
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({fullname, email, password}),
+      body: JSON.stringify({ fullname, email, password }),
     });
-        const data = await res.json();
+    const data = await res.json();
     if (!res.ok) {
       throw new Error(data.message || "Đăng ký thất bại");
-    }else {
-      window.location.href = "/";
-    }
+    } 
 
     return data;
-  }
+  },
+
+  getOTPVerify: async (email) => {
+    try {
+      const res = await fetch(`${API_URL}/email`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      });
+      if (!res.ok) {
+        const errorData = await res.json();
+        throw new Error(errorData.message || "Lấy OTP thất bại");
+      }
+
+      const data = await res.json();
+      return data;
+    } catch (error) {
+      console.error("Lỗi khi gọi getOTPVerify:", error);
+      throw error;
+    }
+  },
+  
+  verifyOTP: async (email, otp) => {
+    const res = await fetch(`${API_URL}/verify`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, otp }),
+    });
+    const data = await res.json();
+    if (!res.ok) {
+      throw new Error(data.message || "Xác thực OTP thất bại");
+    } 
+
+    return data;
+  },
 };
