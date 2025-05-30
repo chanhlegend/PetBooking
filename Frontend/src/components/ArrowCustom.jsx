22bd8f54e097c5a03df9e724777038c0d20c61dc
Minh Hiển <minhhien30201@gmail.com>
2025-05-30 20:17:39 +0700

    update home

--- Frontend/src/components/ArrowCustom.jsx
+++ Frontend/src/components/ArrowCustom.jsx
@@ -3,8 +3,14 @@ import { ArrowForwardIos, ArrowBackIos } from "@mui/icons-material";
 function NextArrow(props) {
   const { onClick } = props;
   return (
-    <div onClick={onClick} className="absolute right-4 top-1/2 z-10 cursor-pointer">
-      <ArrowForwardIos style={{ color: "#c4662e" }} />
+    <div
+      onClick={onClick}
+      className="absolute right-4 top-1/2 z-10 cursor-pointer w-10 h-10 flex items-center justify-center rounded-full border border-orange-500 hover:bg-orange-100"
+    >
+      <ArrowForwardIos
+        fontSize="small"
+        style={{ color: "#c4662e", fontSize: "20px" }}
+      />
     </div>
   );
 }
@@ -12,8 +18,14 @@ function NextArrow(props) {
 function PrevArrow(props) {
   const { onClick } = props;
   return (
-    <div onClick={onClick} className="absolute left-4 top-1/2 z-10 cursor-pointer">
-      <ArrowBackIos style={{ color: "#c4662e" }} />
+    <div
+      onClick={onClick}
+      className="absolute left-4 top-1/2 z-10 cursor-pointer w-10 h-10 flex items-center justify-center rounded-full border border-orange-500 hover:bg-orange-100"
+    >
+      <ArrowBackIos 
+        fontSize="small"
+        style={{ color: "#c4662e", fontSize: "20px", paddingLeft: "4px" }}
+      />
     </div>
   );
 }
